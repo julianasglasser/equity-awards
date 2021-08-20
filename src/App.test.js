af5900cdc726bcb266386a3b723e19579889845f
julianasglasser <juliana.schneider.ime@gmail.com>
2021-08-20 13:23:03 -0300

    Fix the app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders table header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Vesting Date/i);
+  expect(headerElement).toBeInTheDocument();
 });
