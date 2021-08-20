cca4c5cfedc45c2a9a087c38d4c210fd3e350f9d
julianasglasser <juliana.schneider.ime@gmail.com>
2021-08-20 11:45:05 -0300

    Fix indentation

--- src/components/VestInfo/VestInfo.jsx
+++ src/components/VestInfo/VestInfo.jsx
@@ -38,4 +38,4 @@ export default function VestInfo({ props }) {
             </table>
         </div>
     );
-  }
+}
--- src/components/VestNav/VestNav.jsx
+++ src/components/VestNav/VestNav.jsx
@@ -3,29 +3,29 @@ import './vestNav.scss';
 import VestInfo from '../VestInfo';
 
 export default function VestNav({ data }) {
-    const [state, setstate] = useState({active: 0});
+    const [state, setstate] = useState({ active: 0 });
 
     const changeState = (evt) => {
-        setstate({active: evt.target.id}); 
+        setstate({ active: evt.target.id });
     };
 
     return (
         <div className="vestNav">
             <ul>
                 {data.map((vest, index) => (
-                <li key={index}>
-                    <button 
-                        className={Number(state.active) === index ? 'active' : 'inactive'}
-                        id={index}
-                        onClick={changeState}
-                        type="button"
+                    <li key={index}>
+                        <button
+                            className={Number(state.active) === index ? 'active' : 'inactive'}
+                            id={index}
+                            onClick={changeState}
+                            type="button"
                         >
-                        {vest.label}
-                    </button>
-                </li>
+                            {vest.label}
+                        </button>
+                    </li>
                 ))}
             </ul>
-            <VestInfo props={data[state.active]}/>
+            <VestInfo props={data[state.active]} />
         </div>
     );
 }
